000d64e20727e8934942f43b3c242af7abe8edd2
mahfudz <2020971151@isiswa.uit,.edu.my>
2020-10-25 15:35:06 +0000

    2.2.c adding <sys/wait.h> and wait(NULL)

--- 2.2.c
+++ 2.2.c
@@ -1,6 +1,7 @@
 #include <stdlib.h> /*needed to define exit*/
 #include <unistd.h> /*needed for fork() and getpid()*/
 #include <stdio.h> /*needed for printf()*/
+#include <sys/wait.h>
 
 int main(int argc, char **argv)
 {
@@ -13,6 +14,7 @@ int main(int argc, char **argv)
 		break;
 
 		default: //a fork return a pid to the parent
+		wait(NULL);
 		printf("I am the parent process: pid=%d, child pid=%d\n",getpid(),pid);
 		break;
 
